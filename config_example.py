beea716165c210f0d420d4d176ebe955e301cdcc
bigbastik <bigbastik@protonmail.ch>
2025-10-27 14:48:20 +0100

    Create config_example.py

---
+++ config_example.py
@@ -0,0 +1,10 @@
+SERVER = "zinc.libera.chat"
+PORT = 6667
+NICKNAME = "saluto_boy"
+REALNAME = "SalutoBot"
+CHANNELS = ["#liberachat"]
+PM_MESSAGE_TEMPLATE = "Ciao {user}! https://lottobot.it 😊"
+MESSAGE_INTERVAL = 3  # secondi tra un messaggio e l'altro
+
+USE_NICKSERV_AUTH = True
+NICKSERV_PASSWORD = "inserisci_la_tua_password"  # NON usare la password reale qui!
